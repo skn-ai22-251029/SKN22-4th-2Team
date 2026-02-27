afad9231aeb12301fc5bca0297551ec779c741aa
Seunghyuk Han <gksshing@users.noreply.github.com>
2026-02-28 02:01:20 +0900

    fix: bm25_params_path 속성 누락 복원
    이전 커밋에서 self.bm25_params_path = INDEX_DIR / 'bm25_params.json'
    할당문이 실수로 삭제됨 -> AttributeError 발생
    
    Fixes: 'PineconeClient' object has no attribute 'bm25_params_path'

--- src/vector_db.py
+++ src/vector_db.py
@@ -196,6 +196,7 @@ class PineconeClient:
         # BM25Encoder 셋업: HuggingFace 기반 모델 다운로드 시 HOME 디렉토리에
         # 쓰기를 시도하므로, 컨테이너 환경에서 Permission denied를 방지하기 위해
         # HOME과 캐시 경로를 /tmp로 임시 변경합니다.
+        self.bm25_params_path = INDEX_DIR / "bm25_params.json"
         import os as _os
         _original_home = _os.environ.get("HOME", "")
         _os.environ["HOME"] = "/tmp"
